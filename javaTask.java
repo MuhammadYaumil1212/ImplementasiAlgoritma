1cda704417bcf104383da0cd5578cb2d44dc080f
MuhammadYaumil1212 <yaumilramkhadani@gmail.com>
2020-09-16 09:49:37 +0700

    Tugas Java Implementasi Algoritma

---
+++ javaTask.java
@@ -0,0 +1,12 @@
+public class javaTask {
+    public static void main(String[] args) {
+        System.out.println("NIM : 6706200001");
+        System.out.println("NAMA : MUHAMMAD YAUMIL RAMADHANI");
+        System.out.println("KELAS :D3IF-44-01");
+
+        int panjang = 10;
+        int lebar = 5;
+        int hasil = panjang * lebar;
+        System.out.print("Hasil dari panjang * lebar = " + hasil);
+    }
+}
